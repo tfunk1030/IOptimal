6f1f38da5908c4615e5ffe1f8c229890793fa23b
tfunk1030 <tfunk1030@gmail.com>
2026-03-13 13:30:33 -0500

    asl

--- solver/solve.py
+++ solver/solve.py
@@ -287,4 +287,6 @@ def main():
 
 
 if __name__ == "__main__":
+    sys.stdout.reconfigure(encoding="utf-8", errors="replace")
+    sys.stderr.reconfigure(encoding="utf-8", errors="replace")
     main()
